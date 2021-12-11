0b2f44ff1f5b87aa54f37dfdc74f3301b9278a1b
Jellef Abbenseth <jellef@gmx.de>
2021-12-11 12:49:23 +0100

    Test create and display animals, staff and compound

--- program/src/zoo_administration.java
+++ program/src/zoo_administration.java
@@ -2,8 +2,21 @@ public class zoo_administration {
     public static void main(String[] args) {
         System.out.println("Programmstart!");
 
-        classes.animals lion = new classes.animals(1, 5, 'm', "Säugetier", "Löwe", 5,
-                0, );
+        classes.animals[] animals = {new classes.animals(1, 5, 'm', "Säugetier",
+                "Löwe", 5, 0, new int[]{})};
 
+        animals[0].display();
+        System.out.println("---------------------------------");
+
+        classes.staff[] mitarbeiter = {new classes.staff(1, 24,'m',"Tom",
+                "Schneider", "Pflege")};
+        mitarbeiter[0].display();
+        System.out.println("---------------------------------");
+
+        classes.compound[] compounds = {new classes.compound(1, "Raubtierhaus",
+                5, new String[]{"Pflege", "Fütterung"})};
+        compounds[0].display();
+
+        System.out.println("Programmende!");
     }
 }
